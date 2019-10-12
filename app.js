ee188feb7e24281a15dc4ebe947b804ffc8a2f05
thanhlong <info@coders.tokyo>
2019-10-12 08:08:00 +0700

    Install-setup

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 var mongoose= require('mongoose');
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect(process.env.MONGO_URL,{useUnifiedTopology: true,
-//   useNewUrlParser: true, })
-//  const db = mongoose.connection
-//  db.on('error', error => console.error(error))
-//  db.once('open', () => console.log('Connected to Mongoose'))
+// mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true });
+ mongoose.connect(process.env.DATABASE_URL,{useUnifiedTopology: true,
+   useNewUrlParser: true, })
+ const db = mongoose.connection
+  db.on('error', error => console.error(error))
+  db.once('open', () => console.log('Connected to Mongoose'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
