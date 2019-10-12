8a072080efd42e253022dd092fddd5b280f9556b
thanhlong <info@coders.tokyo>
2019-10-12 11:21:47 +0700

    make it better

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var port = 1211;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
